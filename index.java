f0d10fda3c15d8a9811698f21a98c6968c371bd4
Arthur Hermes <89710458+ArthurHermes@users.noreply.github.com>
2024-04-05 08:24:22 -0300

    Create index

---
+++ index.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class index{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-------------Bem-Vindo-------------");
+
+
+        //1 - Cadastrar Usuario
+        //2 - Cadastrar Insumos
+        //3 - Consultar insumos
+        //4 - Criar pedido
+        //0 - Sair
+
+        System.out.println("Selecione a baixo a oçpção que desejar");
+
+        while(true){
+            System.out.println("1 - Cadastrar Usúario");
+        }
+    }
+}
