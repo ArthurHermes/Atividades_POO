5cfb36c969efcc03f0e187b08e754d214084eb90
Ricardo Amaro <147277909+Refilx1@users.noreply.github.com>
2024-04-12 09:01:20 -0300

    Update Acessorio.java

--- PBL06/Acessorio.java
+++ PBL06/Acessorio.java
@@ -5,13 +5,15 @@ public class Acessorio extends Produto{
 }
 
 class Bone extends Produto{
-    private String tp_aba; // Exemplo: Reta, Curvada //
+    private String tpAba; // Exemplo: Reta, Curvada //
+    private String tpFecho;
 }
 
 class Mochila extends Produto{
-    private String tp_mochila; // Exemplo: Mochila com rodinha, Mochila de costas, Mochila de  //
+    private String tpMochila; // Exemplo: Mochila com rodinha, Mochila de costas, Mochila de  //
 }
 
 class Meia extends Produto{
-    private String tp_meia; // Exemplo: Cano alto, Cano medio, Cano Alto //
+    private String tpMeia; // Exemplo: Cano alto, Cano medio, Cano Alto //
+    private String estacaoMeia; // Inverno verão 
 }
