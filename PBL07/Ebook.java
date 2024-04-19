01da0ff05620448b233b1b27c7f47de3c4840f58
Ricardo Amaro <147277909+Refilx1@users.noreply.github.com>
2024-04-19 08:54:12 -0300

    Update Ebook.java

--- PBL07/Ebook.java
+++ PBL07/Ebook.java
@@ -1,6 +1,16 @@
 package PBL07;
 
-public class Ebook extends ItemBibliotecaDigital implements {
-    
+public class Ebook extends ItemBibliotecaDigital implements Baixavel {
+    @Override
+    public String descricao() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void baixar() {
+        // TODO Auto-generated method stub
+        
+    }
 }
 
