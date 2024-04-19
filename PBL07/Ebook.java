12f74b5e3ef825dad4998492ce370e79928c305b
ValArndt <142906934+ValArndt@users.noreply.github.com>
2024-04-19 08:54:34 -0300

    ebook

--- PBL07/Ebook.java
+++ PBL07/Ebook.java
@@ -1,6 +1,22 @@
 package PBL07;
 
-public class Ebook extends ItemBibliotecaDigital implements {
-    
+public class Ebook extends ItemBibliotecaDigital implements Baixavel {
+    private String formato;
+    private int tamanhoMB;
+
+    public Ebook(String titulo, String autor, String formato, int tamanhoMB) {
+        super(titulo, autor);
+        this.autor = autor;
+        this.formato = formato;
+        this.tamanhoMB = tamanhoMB;
+    }
+    public String descricao(){
+        return "Nome"+ titulo +"autor"+ autor;
+
+    }
+    public void baixar(){
+        System.out.println("baixando" + titulo);
+    }
+
 }
 
