852543bffbd06d8b483a19dbdd5eeed51fffb0e7
Arthur Hermes <89710458+ArthurHermes@users.noreply.github.com>
2024-04-19 08:53:56 -0300

    a

--- PBL07/main.java
+++ PBL07/main.java
@@ -1,9 +1,10 @@
+package PBL07;
+
 public class main {
     public static void main(String[] args) {
-        Ebook ebook = new Ebook("POO", "marina", "teste");
-        VideoDigital video = new VideoDigital("the walking dead", "ricardo", "teste")
+        //Ebook ebook = new Ebook("POO", "marina", "teste");
+        //VideoDigital video = new VideoDigital("the walking dead", "ricardo", "teste");
 
-    System.out.println("descrição do ebook");
-    System.out.println();
+        
     }
 }
