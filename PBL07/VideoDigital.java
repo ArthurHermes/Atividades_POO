a4d9081da858215e477b8becd004a1cd0437e054
ValArndt <142906934+ValArndt@users.noreply.github.com>
2024-04-19 08:42:36 -0300

    sad

--- PBL07/VideoDigital.java
+++ PBL07/VideoDigital.java
@@ -1,5 +1,25 @@
-package PBL07;
-
-public class VideoDigital extends ItemBibliotecaDigital{
+class VideoDigital extends ItemBibliotecaDigital implements Baixavel, Visualizavel {
+    private String formato;
+    private int resolucao;
     
+    public VideoDigital(String titulo, String autor, String formato, int resolucao) {
+        super(titulo, autor);
+        this.formato = formato;
+        this.resolucao = resolucao;
+    }
+    
+    @Override
+    public String descricao() {
+        return "Video Digital: " + titulo + " - Autor: " + autor + " - Formato: " + formato + " - Resolução: " + resolucao + "p";
+    }
+    
+    @Override
+    public void baixar() {
+        System.out.println("Baixando o vídeo digital: " + titulo);
+    }
+    
+    @Override
+    public void visualizar() {
+        System.out.println("Visualizando o vídeo digital: " + titulo);
+    }
 }
