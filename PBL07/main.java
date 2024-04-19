05763bbefbd3184defe0060e1839cd8b5a69915f
ValArndt <142906934+ValArndt@users.noreply.github.com>
2024-04-19 09:12:29 -0300

    valdemar

--- PBL07/main.java
+++ PBL07/main.java
@@ -2,8 +2,8 @@ package PBL07;
 
 public class Main {
     public static void main(String[] args) {
-        Ebook ebook = new Ebook("marina", "poo", "teste");
-        VideoDigital video = new VideoDigital("poo", "teste", "teste");
+        Ebook ebook = new Ebook("marina", "poo");
+        VideoDigital video = new VideoDigital("poo", "teste");
         
     }
 }
